7bcc7d9ba3205a46af01bf6ae404b8404c07777c
dk9457 <107979351+dk9457@users.noreply.github.com>
2022-06-22 14:47:11 +0530

    hii

---
+++ script.js
@@ -0,0 +1,5 @@
+let home=Array.from(document.getElementsByClassName("home"));
+home[0].addEventListener("click",()=>{
+let picc=Array.from(document.getElementsByClassName("picc"));
+    picc[0].classList.remove("picc");
+})
